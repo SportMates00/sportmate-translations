efdcf8966b32bffb8a447c179132cf3946cf88ac
SportMates00 <fortunecasateam@gmail.com>
2026-05-29 16:46:08 +0400

    Create SportsAndActivities.jsx

---
+++ locales/en/SportsAndActivities.jsx
@@ -0,0 +1,9 @@
+{
+  "FilterByCategory": "Filter By Category",
+  "EventClear": "Clear",
+  "EventSports": "Sports",
+  "EventActivities": "Activities",
+  "CustomActivity": " Custom Activity",
+  "EventCancelFilter": "Cancel",
+  "EventApplyFilter": "Apply"
+}
